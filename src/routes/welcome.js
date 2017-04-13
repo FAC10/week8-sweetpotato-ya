2f3f0d09be03916d294385d258ec2922ff6dc7fb
antoniotrkdz <antoniotrkdz@gmail.com>
2017-04-13 10:52:06 +0100

    Add working cookie set() relates #26

--- src/routes/welcome.js
+++ src/routes/welcome.js
@@ -2,6 +2,7 @@ const request = require('request');
 const querystring = require('querystring');
 const env = require('env2')('./config.env');
 const dbConnection = require('../../database/db_connection.js');
+const credentials = require('hapi-context-credentials');
 
 
 module.exports = {
@@ -12,7 +13,7 @@ module.exports = {
       console.log(body);
       const accessToken = querystring.parse(body).access_token;
       const headers = {
-        'User-Agent': 'week8-MZ',
+        'User-Agent': 'week8-YA',
         Authorization: `token ${accessToken}`,
       };
       request.get({ url: 'https://api.github.com/user', headers }, (error, response, bodyString) => {
@@ -22,12 +23,19 @@ module.exports = {
         const sqlQuery = `INSERT INTO users (username, avatar_url, github_id, access_token) VALUES ('${bodyObject.login}', '${bodyObject.avatar_url}', ${bodyObject.id}, '${accessToken}') ON CONFLICT (github_id) DO UPDATE SET username = excluded.username, avatar_url = excluded.avatar_url, access_token = excluded.access_token;`;
         dbConnection.query(sqlQuery, (err, res) => {
           if (err) throw err;
-          req.cookieAuth.set({
-            username: bodyObject.login,
-            avatar: bodyObject.avatar_url,
-            accessToken,
-          });
-          console.log(bodyObject.login, '<<<<<<<<<<<');
+
+          // req.cookieAuth.set({
+          //   username: bodyObject.login,
+          //   avatar: bodyObject.avatar_url,
+          //   accessToken,
+          // });
+        });
+        console.log(bodyObject.login, '<<<<<<<<<<<');
+        console.log(bodyObject.avatar_url, '<<<<<<<<<<<');
+        req.cookieAuth.set({
+          username: bodyObject.login,
+          avatar: bodyObject.avatar_url,
+          accessToken,
         });
         reply.redirect('/secure', { credentials: req.auth.credentials });
       });
